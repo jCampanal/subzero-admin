cf245b0393a94dfa4ed61c629989519e3290d25a
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-16 00:06:20 -0400

    Redirect "/" to "/dashboard"

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -1,32 +1,33 @@
-import { Redirect } from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 import FuseUtils from '@fuse/utils';
-import ExampleConfig from 'app/main/example/ExampleConfig';
+import DashboardPageConfig from 'app/main/dashboard/DashboardPageConfig';
+import CategoriesPageConfig from 'app/main/categories/CategoriesPageConfig';
 import FuseLoading from '@fuse/core/FuseLoading';
 import Error404Page from 'app/main/404/Error404Page';
 
-const routeConfigs = [ExampleConfig];
+const routeConfigs = [DashboardPageConfig, CategoriesPageConfig];
 
 const routes = [
-  // if you want to make whole app auth protected by default change defaultAuth for example:
+  // if you want to make whole app auth protected by default change defaultAuth for dashboard:
   // ...FuseUtils.generateRoutesFromConfigs(routeConfigs, ['admin','staff','user']),
   // The individual route configs which has auth option won't be overridden.
   ...FuseUtils.generateRoutesFromConfigs(routeConfigs, null),
   {
     exact: true,
     path: '/',
-    component: () => <Redirect to="/example" />,
+    component: () => <Redirect to="/dashboard"/>,
   },
   {
     path: '/loading',
     exact: true,
-    component: () => <FuseLoading />,
+    component: () => <FuseLoading/>,
   },
   {
     path: '/404',
-    component: () => <Error404Page />,
+    component: () => <Error404Page/>,
   },
   {
-    component: () => <Redirect to="/404" />,
+    component: () => <Redirect to="/404"/>,
   },
 ];
 
