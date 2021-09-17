76c53e5b6d98cd70ca944b6b5ac21311b6373954
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-17 05:23:26 -0400

    Updated the link for Emails card

--- src/app/main/dashboard/Dashboard/Dashboard.js
+++ src/app/main/dashboard/Dashboard/Dashboard.js
@@ -128,7 +128,7 @@ function Dashboard() {
         </div>
         <div className="shadow-lg">
           <Link
-            to="/categories"
+            to="/emails"
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
