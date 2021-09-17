db0a0f1b9aa8489966597dd2f4d729b1fd539360
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-17 01:20:47 -0400

    Updated the link for the CoolersActivity card

--- src/app/main/dashboard/Dashboard/Dashboard.js
+++ src/app/main/dashboard/Dashboard/Dashboard.js
@@ -38,7 +38,7 @@ function Dashboard() {
         </div>
         <div className="shadow-lg">
           <Link
-            to="/categories"
+            to="/coolers-activity"
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
