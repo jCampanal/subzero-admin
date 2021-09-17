c59f1b8a3867c5e34077d0466ccef787d1121b24
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-17 03:43:10 -0400

    Updated the link for Admins card

--- src/app/main/dashboard/Dashboard/Dashboard.js
+++ src/app/main/dashboard/Dashboard/Dashboard.js
@@ -108,7 +108,7 @@ function Dashboard() {
         </div>
         <div className="shadow-lg">
           <Link
-            to="/categories"
+            to="/admins"
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
