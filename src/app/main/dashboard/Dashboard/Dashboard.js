d682d97696d6c1671f0eaffeabaf474b38eec6f9
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-16 23:53:30 -0400

    Updated the link of Coolers card

--- src/app/main/dashboard/Dashboard/Dashboard.js
+++ src/app/main/dashboard/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import {memo} from 'react';
-import {Link} from 'react-router-dom';
+import { memo } from 'react';
+import { Link } from 'react-router-dom';
 
 function Dashboard() {
   return (
@@ -12,7 +12,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-list block"/>
+            <i className="fa fa-4x fa-list block" />
             <span className="h5 mt-10">Categories</span>
           </Link>
         </div>
@@ -22,17 +22,17 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-shopping-cart block"/>
+            <i className="fa fa-4x fa-shopping-cart block" />
             <span className="h5 mt-10">Products</span>
           </Link>
         </div>
         <div className="shadow-lg">
           <Link
-            to="/categories"
+            to="/coolers"
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-box block"/>
+            <i className="fa fa-4x fa-box block" />
             <span className="h5 mt-10">Coolers</span>
           </Link>
         </div>
@@ -42,7 +42,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-history block"/>
+            <i className="fa fa-4x fa-history block" />
             <span className="h5 mt-10">Coolers Activity</span>
           </Link>
         </div>
@@ -52,7 +52,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-user block"/>
+            <i className="fa fa-4x fa-user block" />
             <span className="h5 mt-10">Customers</span>
           </Link>
         </div>
@@ -62,7 +62,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-calendar block"/>
+            <i className="fa fa-4x fa-calendar block" />
             <span className="h5 mt-10">Schedules</span>
           </Link>
         </div>
@@ -72,7 +72,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-truck block"/>
+            <i className="fa fa-4x fa-truck block" />
             <span className="h5 mt-10">Drivers</span>
           </Link>
         </div>
@@ -82,7 +82,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-truck-loading block"/>
+            <i className="fa fa-4x fa-truck-loading block" />
             <span className="h5 mt-10">Shipments</span>
           </Link>
         </div>
@@ -92,7 +92,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-inbox block"/>
+            <i className="fa fa-4x fa-inbox block" />
             <span className="h5 mt-10">Order admin</span>
           </Link>
         </div>
@@ -102,7 +102,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-warehouse block"/>
+            <i className="fa fa-4x fa-warehouse block" />
             <span className="h5 mt-10">Warehouses</span>
           </Link>
         </div>
@@ -112,7 +112,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-user block"/>
+            <i className="fa fa-4x fa-user block" />
             <span className="h5 mt-10">Admins</span>
           </Link>
         </div>
@@ -122,7 +122,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-ban block"/>
+            <i className="fa fa-4x fa-ban block" />
             <span className="h5 mt-10">Black list</span>
           </Link>
         </div>
@@ -132,7 +132,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-envelope block"/>
+            <i className="fa fa-4x fa-envelope block" />
             <span className="h5 mt-10">Emails</span>
           </Link>
         </div>
@@ -145,7 +145,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-tasks block"/>
+            <i className="fa fa-4x fa-tasks block" />
             <span className="h5 mt-10">Order monitor</span>
           </Link>
         </div>
@@ -155,7 +155,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-tasks block"/>
+            <i className="fa fa-4x fa-tasks block" />
             <span className="h5 mt-10">Order monitor 2</span>
           </Link>
         </div>
@@ -165,7 +165,7 @@ function Dashboard() {
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
-            <i className="fa fa-4x fa-map-marked block"/>
+            <i className="fa fa-4x fa-map-marked block" />
             <span className="h5 mt-10">Driver monito</span>
           </Link>
         </div>
