59a1887180e88eba8385375ed7425af0389991c4
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-17 03:31:04 -0400

    Updated the link of Warehouses card

--- src/app/main/dashboard/Dashboard/Dashboard.js
+++ src/app/main/dashboard/Dashboard/Dashboard.js
@@ -98,7 +98,7 @@ function Dashboard() {
         </div>
         <div className="shadow-lg">
           <Link
-            to="/categories"
+            to="/warehouses"
             className="flex flex-col bg-gray-100 hover:bg-gray-800 text-gray-900 hover:text-gray-50 items-center mx-auto py-20"
             role="button"
           >
