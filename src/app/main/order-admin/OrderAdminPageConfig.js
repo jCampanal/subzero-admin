8c5d6f6ee8ed908487245cec3c22f4c34b0a46dc
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-20 16:35:39 -0400

    Components for the OrderAdmin page

---
+++ src/app/main/order-admin/OrderAdminPageConfig.js
@@ -0,0 +1,25 @@
+import i18next from 'i18next';
+import OrderAdminPage from './OrderAdminPage';
+import en from './i18n/en';
+import tr from './i18n/tr';
+import ar from './i18n/ar';
+
+i18next.addResourceBundle('en', 'examplePage', en);
+i18next.addResourceBundle('tr', 'examplePage', tr);
+i18next.addResourceBundle('ar', 'examplePage', ar);
+
+const OrderAdminPageConfig = {
+    settings: {
+        layout: {
+            config: {},
+        },
+    },
+    routes: [
+        {
+            path: '/order-admin',
+            component: OrderAdminPage,
+        },
+    ],
+};
+
+export default OrderAdminPageConfig;
