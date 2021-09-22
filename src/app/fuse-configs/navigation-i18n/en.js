41391b5e873701e7ff341f0aca1bb9604dc2afd4
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-21 23:18:55 -0400

    Added the sections links to the sliding sidebar

--- src/app/fuse-configs/navigation-i18n/en.js
+++ src/app/fuse-configs/navigation-i18n/en.js
@@ -1,6 +1,22 @@
 const locale = {
-  APPLICATIONS: 'Applications',
-  EXAMPLE: 'Example',
+    ADMINISTRATION: 'Administration',
+    CATEGORIES: 'Categories',
+    PRODUCTS: 'Products',
+    COOLERS: 'Coolers',
+    COOLERS_ACTIVITY: 'Coolers Activity',
+    CUSTOMERS: 'Customers',
+    SCHEDULES: 'Schedules',
+    DRIVERS: 'Drivers',
+    SHIPMENTS: 'Shipments',
+    ORDERS_ADMIN: 'Orders Admin',
+    WAREHOUSES: 'Warehouses',
+    ADMINS: 'Admins',
+    BLACKLIST: 'BlackList',
+    EMAILS: 'Emails',
+    MONITORS: 'Monitors',
+    ORDERS_MONITOR: 'Orders Monitor',
+    ORDERS_CALENDAR: 'Orders Calendar',
+    DRIVERS_MONITOR: 'Drivers Monitor',
 };
 
 export default locale;
