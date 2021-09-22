5218f85654c0071e27419a00b236e0ab5b50b0e6
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-22 03:00:31 -0400

    Removed unused modules and files

--- src/app/store/fuse/navigationSlice.js
+++ src/app/store/fuse/navigationSlice.js
@@ -1,4 +1,4 @@
-import { createSelector, createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import {createSelector, createEntityAdapter, createSlice} from '@reduxjs/toolkit';
 import navigationConfig from 'app/fuse-configs/navigationConfig';
 import FuseUtils from '@fuse/utils';
 import i18next from 'i18next';
@@ -9,80 +9,71 @@ const emptyInitialState = navigationAdapter.getInitialState();
 const initialState = navigationAdapter.upsertMany(emptyInitialState, navigationConfig);
 
 export const appendNavigationItem = (item, parentId) => (dispatch, getState) => {
-  const navigation = selectNavigationAll(getState());
+    const navigation = selectNavigationAll(getState());
 
-  return dispatch(setNavigation(FuseUtils.appendNavItem(navigation, item, parentId)));
+    return dispatch(setNavigation(FuseUtils.appendNavItem(navigation, item, parentId)));
 };
 
 export const prependNavigationItem = (item, parentId) => (dispatch, getState) => {
-  const navigation = selectNavigationAll(getState());
+    const navigation = selectNavigationAll(getState());
 
-  return dispatch(setNavigation(FuseUtils.prependNavItem(navigation, item, parentId)));
+    return dispatch(setNavigation(FuseUtils.prependNavItem(navigation, item, parentId)));
 };
 
 export const updateNavigationItem = (id, item) => (dispatch, getState) => {
-  const navigation = selectNavigationAll(getState());
+    const navigation = selectNavigationAll(getState());
 
-  return dispatch(setNavigation(FuseUtils.updateNavItem(navigation, id, item)));
+    return dispatch(setNavigation(FuseUtils.updateNavItem(navigation, id, item)));
 };
 
 export const removeNavigationItem = (id) => (dispatch, getState) => {
-  const navigation = selectNavigationAll(getState());
+    const navigation = selectNavigationAll(getState());
 
-  return dispatch(setNavigation(FuseUtils.removeNavItem(navigation, id)));
+    return dispatch(setNavigation(FuseUtils.removeNavItem(navigation, id)));
 };
 
 export const {
-  selectAll: selectNavigationAll,
-  selectIds: selectNavigationIds,
-  selectById: selectNavigationItemById,
+    selectAll: selectNavigationAll,
+    selectIds: selectNavigationIds,
+    selectById: selectNavigationItemById,
 } = navigationAdapter.getSelectors((state) => state.fuse.navigation);
 
 const navigationSlice = createSlice({
-  name: 'navigation',
-  initialState,
-  reducers: {
-    setNavigation: navigationAdapter.setAll,
-    resetNavigation: (state, action) => initialState,
-  },
+    name: 'navigation',
+    initialState,
+    reducers: {
+        setNavigation: navigationAdapter.setAll,
+        resetNavigation: (state, action) => initialState,
+    },
 });
 
-export const { setNavigation, resetNavigation } = navigationSlice.actions;
+export const {setNavigation, resetNavigation} = navigationSlice.actions;
 
-const getUserRole = (state) => state.auth.user.role;
-
-export const selectNavigation = createSelector(
-  [selectNavigationAll, ({ i18n }) => i18n.language, getUserRole],
-  (navigation, language, userRole) => {
+export const selectNavigation = createSelector([selectNavigationAll, ({i18n}) => i18n.language], (navigation, language, userRole) => {
     function setTranslationValues(data) {
-      // loop through every object in the array
-      return data.map((item) => {
-        if (item.translate && item.title) {
-          item.title = i18next.t(`navigation:${item.translate}`);
-        }
+        // loop through every object in the array
+        return data.map((item) => {
+            if (item.translate && item.title) {
+                item.title = i18next.t(`navigation:${item.translate}`);
+            }
 
-        // see if there is a children node
-        if (item.children) {
-          // run this function recursively on the children array
-          item.children = setTranslationValues(item.children);
-        }
-        return item;
-      });
+            // see if there is a children node
+            if (item.children) {
+                // run this function recursively on the children array
+                item.children = setTranslationValues(item.children);
+            }
+            return item;
+        });
     }
 
     return setTranslationValues(
-      _.merge(
-        [],
-        FuseUtils.filterRecursive(navigation, (item) =>
-          FuseUtils.hasPermission(item.auth, userRole)
+        _.merge(
+            [],
+            FuseUtils.filterRecursive(navigation, (item) => FuseUtils.hasPermission(item.auth, userRole))
         )
-      )
     );
-  }
-);
+});
 
-export const selectFlatNavigation = createSelector([selectNavigation], (navigation) =>
-  FuseUtils.getFlatNavigation(navigation)
-);
+export const selectFlatNavigation = createSelector([selectNavigation], (navigation) => FuseUtils.getFlatNavigation(navigation));
 
 export default navigationSlice.reducer;
--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,24 +1,15 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import auth from 'app/auth/store';
+import {combineReducers} from '@reduxjs/toolkit';
 import fuse from './fuse';
 import i18n from './i18nSlice';
 
 const createReducer = (asyncReducers) => (state, action) => {
-  const combinedReducer = combineReducers({
-    auth,
-    fuse,
-    i18n,
-    ...asyncReducers,
-  });
+    const combinedReducer = combineReducers({
+        fuse,
+        i18n,
+        ...asyncReducers,
+    });
 
-  /*
-	Reset the redux store when user logged out
-	 */
-  if (action.type === 'auth/user/userLoggedOut') {
-    state = undefined;
-  }
-
-  return combinedReducer(state, action);
+    return combinedReducer(state, action);
 };
 
 export default createReducer;
