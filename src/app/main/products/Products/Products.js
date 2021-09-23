dd2347683036d75e68417d39ac875e6a994e1ddf
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-23 01:40:07 -0400

    This component now uses FusePageCarded to render ProductsTable

--- src/app/main/products/Products/Products.js
+++ src/app/main/products/Products/Products.js
@@ -1,29 +1,157 @@
-import {lazy, memo, useState} from 'react';
-import {ShoppingCart} from '@material-ui/icons';
-import Button from '@material-ui/core/Button';
+import FusePageCarded from "@fuse/core/FusePageCarded";
+import { lazy, memo, useState } from "react";
 
-const ProductForm = lazy(() => import('./ProductForm'));
-const ConfirmDlg = lazy(() => import('./ConfirmDlg'));
+const Header = lazy(() => import("./PageCardedHeader"));
+const ProductsTable = lazy(() => import("./ProductsTable"));
+const ProductForm = lazy(() => import("./ProductForm"));
+const ConfirmDlg = lazy(() => import("./ConfirmDlg"));
+
+const rows = [
+    {
+        id: 'image',
+        align: 'left',
+        disablePadding: true,
+        label: '',
+        sort: false,
+    },
+    {
+        id: 'name',
+        align: 'left',
+        disablePadding: false,
+        label: 'Name',
+        sort: true,
+    },
+    {
+        id: 'category',
+        align: 'left',
+        disablePadding: false,
+        label: 'Category',
+        sort: true,
+    },
+    {
+        id: 'units',
+        align: 'right',
+        disablePadding: false,
+        label: 'Units',
+        sort: true,
+    },
+    {
+        id: 'visible',
+        align: 'right',
+        disablePadding: false,
+        label: 'Visible',
+        sort: true,
+    },
+    {
+        id: 'actions',
+        align: 'right',
+        disablePadding: false,
+        label: '',
+        sort: false,
+    },
+];
 
 const dummyProducts = [
-    {id: 1, category: 'Lorem', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 2, category: 'Dolor', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 3, category: 'Ipsum', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 4, category: 'Dolor', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 5, category: 'Dolor', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 6, category: 'Lorem', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 7, category: 'Ipsum', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 8, category: 'Ipsum', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
-    {id: 9, category: 'Lorem', name: 'Grep', units: 'Gue Ueg Egu Geu', visible: true},
+    {
+        id: 1,
+        category: "Lorem",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 2,
+        category: "Dolor",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 3,
+        category: "Ipsum",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 4,
+        category: "Dolor",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 5,
+        category: "Dolor",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 6,
+        category: "Lorem",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 7,
+        category: "Ipsum",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 8,
+        category: "Ipsum",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 9,
+        category: "Lorem",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 10,
+        category: "Lorem",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
+    {
+        id: 11,
+        category: "Lorem",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: false,
+    },
+    {
+        id: 12,
+        category: "Lorem",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: false,
+    },
+    {
+        id: 13,
+        category: "Lorem",
+        name: "Grep",
+        units: "Gue Ueg Egu Geu",
+        visible: true,
+    },
 ];
 
 function Products() {
     const [formIsOpen, openForm] = useState(false);
     const [product, selectProduct] = useState({
         id: 0,
-        category: '',
-        name: '',
-        units: '',
+        category: "",
+        name: "",
+        units: "",
         visible: false,
     });
     const [products, setProducts] = useState(dummyProducts);
@@ -40,7 +168,7 @@ function Products() {
     };
     const deleteProduct = (id) => {
         setProducts((prevState) => prevState.filter((item) => item.id !== id));
-        selectProduct({id: 0, category: '', name: '', units: '', visible: false});
+        selectProduct({ id: 0, category: "", name: "", units: "", visible: false });
         toggleConfirmation();
     };
     const saveProduct = (item) => {
@@ -67,77 +195,30 @@ function Products() {
         toggleConfirmation();
     };
     return (
-        <div>
-            <p className="h1">
-                <ShoppingCart className="text-5xl mr-14" />
-                Products <span className="text-gray-50 rounded-full px-16 bg-blue-600">{dummyProducts.length}</span>
-            </p>
-            <div className="flex justify-center items-center min-w-full mb-14">
-                <Button variant="contained" className="bg-blue-500 hover:bg-blue-700 text-white" onClick={toggleForm}>
-                    New Product
-                </Button>
-            </div>
-            <div className="mt-2 flex flex-col">
-                <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
-                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-100 text-lg font-sans">
-                                <thead className="bg-gray-50 font-medium text-gray-800 font-serif">
-                                    <tr className="text-left tracking-wider border-t border-b">
-                                        <th scope="col" className="px-14 py-3">
-                                            Id
-                                        </th>
-                                        <th scope="col" className="px-14 py-3">
-                                            Category
-                                        </th>
-                                        <th scope="col" className="px-14 py-3">
-                                            Name
-                                        </th>
-                                        <th scope="col" className="px-14 py-3">
-                                            Units
-                                        </th>
-                                        <th scope="col" className="px-14 py-3">
-                                            Visible
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {products.map((item) => (
-                                        <tr key={item.id} className="odd:bg-gray-200 hover:bg-gray-100">
-                                            <td className="px-14 py-3">{item.id}</td>
-                                            <td className="px-14 py-3">{item.category}</td>
-                                            <td className="px-14 py-3">{item.name}</td>
-                                            <td className="px-14 py-3">{item.units}</td>
-                                            <td className="px-14 py-3">
-                                                {item.visible ? (
-                                                    <i className="fa fa-check-circle text-green-800" />
-                                                ) : (
-                                                    <i className="fa fa-times-circle text-red-800" />
-                                                )}
-                                            </td>
-                                            <td className="px-14 py-3">
-                                                <div className="flex justify-end items-center">
-                                                    <Button onClick={() => editProduct(item.id)}>
-                                                        <i className="fa fa-edit mr-4" />
-                                                        Edit
-                                                    </Button>
-                                                    <Button onClick={() => confirmBeforeDeleteCategory(item.id)}>
-                                                        <i className="fa fa-trash mr-4" />
-                                                        Delete
-                                                    </Button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <ProductForm open={formIsOpen} handleClose={toggleForm} product={product} save={saveProduct} />
-            <ConfirmDlg open={confirmDelete} handleClose={toggleConfirmation} confirm={deleteProduct} subject={product} />
-        </div>
+        <>
+            <FusePageCarded
+                classes={{
+                    content: "flex",
+                    contentCard: "overflow-hidden",
+                    header: "min-h-72 h-72 sm:h-136 sm:min-h-136",
+                }}
+                header={<Header iconText="shopping_cart" title="Products" addButtonLabel="New Product" searchHint="Search product by name" />}
+                content={<ProductsTable products={dummyProducts} rows={rows} />}
+                innerScroll
+            />
+            <ProductForm
+                open={formIsOpen}
+                handleClose={toggleForm}
+                product={product}
+                save={saveProduct}
+            />
+            <ConfirmDlg
+                open={confirmDelete}
+                handleClose={toggleConfirmation}
+                confirm={deleteProduct}
+                subject={product}
+            />
+        </>
     );
 }
 
