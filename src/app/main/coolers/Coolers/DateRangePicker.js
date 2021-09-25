3db428bea505e10ce6a4d4d31c7cdefb7e509087
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-25 14:42:49 -0400

    A component for picking date ranges

---
+++ src/app/main/coolers/Coolers/DateRangePicker.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import {DatePicker} from '@material-ui/pickers';
+
+function DateRangePicker(props) {
+    const [dialogIsOpen, openDialog] = useState(false);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const toggleDialog = () => {
+        openDialog(!dialogIsOpen);
+    };
+    useEffect(() => openDialog(props.isOpen), [props.isOpen]);
+    return (
+        <Dialog open={dialogIsOpen} onClose={toggleDialog} fullWidth maxWidth="sm">
+            <AppBar position="static" elevation={0}>
+                <Toolbar className="flex w-full">
+                    <Typography variant="subtitle1" color="inherit">
+                        Pick a date range to search in
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <DialogContent classes={{root: 'p-0'}}>
+                <div className="px-16 sm:px-24">
+                    <div className="flex -mx-4">
+                        <DatePicker
+                            label="Start Date"
+                            inputVariant="outlined"
+                            className="mt-8 mb-16 mx-4"
+                            value={startDate}
+                            onChange={(e) => setStartDate(new Date(e))}
+                        />
+                        <DatePicker
+                            label="Due Date"
+                            inputVariant="outlined"
+                            className="mt-8 mb-16 mx-4"
+                            value={endDate}
+                            onChange={(e) => setEndDate(new Date(e))}
+                        />
+                    </div>
+                </div>
+            </DialogContent>
+            <DialogActions className="justify-between px-8 py-16">
+                <div className="px-16">
+                    <Button type="submit" variant="contained" color="secondary" onClick={toggleDialog}>
+                        Cancel
+                    </Button>
+                </div>
+                <div className="px-16">
+                    <Button type="submit" variant="contained" color="secondary" onClick={toggleDialog}>
+                        Search
+                    </Button>
+                </div>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default DateRangePicker;
+
+DateRangePicker.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+};
