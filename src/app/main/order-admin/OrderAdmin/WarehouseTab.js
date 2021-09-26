1f4f4e7ff90ab977eb8fa2790e9abb06aec9e3d6
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-26 02:58:56 -0400

    Refactored the component WarehouseTab to render OrdersAdminTable on each tab

--- src/app/main/order-admin/OrderAdmin/WarehouseTab.js
+++ src/app/main/order-admin/OrderAdmin/WarehouseTab.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {lazy} from 'react';
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
@@ -6,13 +6,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 
-export const ShipmentStatus = {
-    1: {name: 'Shipping', icon: 'fa-truck', tColor: 'blue-700'},
-    2: {name: 'Delivered', icon: 'fa-handshake', tColor: 'green-700'},
-    3: {name: 'Canceled', icon: 'fa-times', tColor: 'red-700'},
-};
+const OrdersAdminTable = lazy(() => import('./OrdersAdminTable'));
 
 function TabPanel(props) {
     const {children, value, index, ...other} = props;
@@ -85,53 +80,7 @@ export default function WarehouseTab(props) {
             <SwipeableViews axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={value} onChangeIndex={handleChangeIndex}>
                 {props.tabItems.map((tabItem) => (
                     <TabPanel key={tabItem.id} value={value} index={tabItem.id - 1} dir={theme.direction}>
-                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table className="min-w-full divide-y divide-gray-100 text-lg font-sans">
-                                <thead className="bg-gray-50 font-medium text-gray-800 font-serif">
-                                    <tr className="text-left tracking-wider border-t border-b">
-                                        <th scope="col" className="px-14 py-3">
-                                            Id
-                                        </th>
-                                        <th scope="col" className="px-14 py-3">
-                                            Company
-                                        </th>
-                                        <th scope="col" className="px-14 py-3">
-                                            Arrive Time
-                                        </th>
-                                        <th scope="col" className="px-14 py-3">
-                                            Status
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {tabItem.orders.map((order) => (
-                                        <tr key={order.id} className="odd:bg-gray-200 hover:bg-gray-100">
-                                            <td className="px-14 py-3">{order.id}</td>
-                                            <td className="px-14 py-3">{order.company}</td>
-                                            <td className="px-14 py-3">{order.arriveTime.toLocaleDateString()}</td>
-                                            <td className="px-14 py-3">
-                                                <span className={`text-${ShipmentStatus[order.status].tColor} text-sm`}>
-                                                    <i className={`fa ${ShipmentStatus[order.status].icon} mr-4`} />
-                                                    {ShipmentStatus[order.status].name}
-                                                </span>
-                                            </td>
-                                            <td className="px-14 py-3">
-                                                <div className="flex justify-end items-center">
-                                                    <Button type="button" className="text-sm text-gray-700 hover:text-gray-800 leading-none rounded-md mr-5">
-                                                        <i className="fa fa-edit mr-4" />
-                                                        Edit
-                                                    </Button>
-                                                    <Button type="button" className="text-sm text-red-700 hover:text-red-800 leading-none rounded-md">
-                                                        <i className="fa fa-trash mr-4" />
-                                                        Delete
-                                                    </Button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+                        <OrdersAdminTable items={tabItem.orders} rows={props.rows} />
                     </TabPanel>
                 ))}
             </SwipeableViews>
@@ -141,4 +90,5 @@ export default function WarehouseTab(props) {
 
 WarehouseTab.propTypes = {
     tabItems: PropTypes.array.isRequired,
+    rows: PropTypes.array.isRequired,
 };
