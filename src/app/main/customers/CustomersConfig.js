64c00c59bbe75fe87548f9f42dc966769dc0c543
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-01 00:39:59 -0400

    Added spanish translations for customers page

--- src/app/main/customers/CustomersConfig.js
+++ src/app/main/customers/CustomersConfig.js
@@ -1,8 +1,10 @@
 import i18next from 'i18next';
 import React from 'react';
 import en from './i18n/en';
+import es from './i18n/es';
 
-i18next.addResourceBundle('en', 'examplePage', en);
+i18next.addResourceBundle('en', 'customers', en);
+i18next.addResourceBundle('es', 'customers', es);
 
 const CustomersConfig = {
     settings: {
