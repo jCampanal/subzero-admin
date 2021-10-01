dbe121797df5be109e5595ada2d8b88e7cc2aed0
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-30 23:54:00 -0400

    Added spanish translations for coolers page

--- src/app/main/coolers/CoolersPageConfig.js
+++ src/app/main/coolers/CoolersPageConfig.js
@@ -1,8 +1,10 @@
 import i18next from 'i18next';
 import React from 'react';
 import en from './i18n/en';
+import es from './i18n/es';
 
-i18next.addResourceBundle('en', 'examplePage', en);
+i18next.addResourceBundle('en', 'coolers', en);
+i18next.addResourceBundle('es', 'coolers', es);
 
 const CoolersPageConfig = {
     settings: {
