7087d91aeb01bcce9ee93c983b36f940b5c79ded
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-01 00:22:21 -0400

    Added spanish translations for coolers activity page

--- src/app/main/coolers-activity/CoolersActivityPageConfig.js
+++ src/app/main/coolers-activity/CoolersActivityPageConfig.js
@@ -1,8 +1,10 @@
 import i18next from 'i18next';
 import React from 'react';
 import en from './i18n/en';
+import es from './i18n/es';
 
-i18next.addResourceBundle('en', 'examplePage', en);
+i18next.addResourceBundle('en', 'coolers-activity', en);
+i18next.addResourceBundle('es', 'coolers-activity', es);
 
 const CoolersActivityPageConfig = {
     settings: {
