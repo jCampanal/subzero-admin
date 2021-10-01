54e1cadd7960b3392cac481145d294c91f29c00e
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-09-30 22:18:04 -0400

    Added the spanish translations for sidebar navigation

--- src/app/fuse-configs/navigationConfig.js
+++ src/app/fuse-configs/navigationConfig.js
@@ -1,7 +1,9 @@
 import i18next from 'i18next';
 import en from './navigation-i18n/en';
+import es from './navigation-i18n/es';
 
 i18next.addResourceBundle('en', 'navigation', en);
+i18next.addResourceBundle('es', 'navigation', es);
 
 const navigationConfig = [
     {
