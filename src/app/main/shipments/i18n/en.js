4a27ac458fb1a20798e703453f647e4f9355ad57
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-01 16:05:31 -0400

    Translated all labels for shipments page

--- src/app/main/shipments/i18n/en.js
+++ src/app/main/shipments/i18n/en.js
@@ -1,5 +1,11 @@
 const locale = {
-  TITLE: 'Example Page',
+    SHIPMENTS: 'Shipments',
+    SHIPMENTS_COUNT: '1 shipment',
+    SHIPMENTS_COUNT_plural: '{{count}} shipments',
+    WAREHOUSE: 'Warehouse',
+    DATE: 'Date',
+    NO_SHIPMENTS: 'There are no shipments to show here',
+    ROWS_PER_PAGE: 'Shipments per page',
 };
 
 export default locale;
