a1077bec8517384826ebe4abe5c13fd90c04527b
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-01 20:05:16 -0400

    Added a prop 'namespace' to translate labels in DateRangePicker component

--- src/app/main/coolers/Coolers/DateRangePicker.js
+++ src/app/main/coolers/Coolers/DateRangePicker.js
@@ -8,8 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 import {DatePicker} from '@material-ui/pickers';
+import {useTranslation} from 'react-i18next';
 
 function DateRangePicker(props) {
+    const {t} = useTranslation(props.namespace);
     const [dialogIsOpen, openDialog] = useState(false);
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
@@ -22,7 +24,7 @@ function DateRangePicker(props) {
             <AppBar position="static" elevation={0}>
                 <Toolbar className="flex w-full">
                     <Typography variant="subtitle1" color="inherit">
-                        Pick a date range to search in
+                        {t('ADVANCED_SEARCH')}
                     </Typography>
                 </Toolbar>
             </AppBar>
@@ -30,14 +32,14 @@ function DateRangePicker(props) {
                 <div className="px-16 sm:px-24">
                     <div className="flex -mx-4">
                         <DatePicker
-                            label="Start Date"
+                            label={t('START_DATE')}
                             inputVariant="outlined"
                             className="mt-8 mb-16 mx-4"
                             value={startDate}
                             onChange={(e) => setStartDate(new Date(e))}
                         />
                         <DatePicker
-                            label="Due Date"
+                            label={t('END_DATE')}
                             inputVariant="outlined"
                             className="mt-8 mb-16 mx-4"
                             value={endDate}
@@ -46,15 +48,15 @@ function DateRangePicker(props) {
                     </div>
                 </div>
             </DialogContent>
-            <DialogActions className="justify-between px-8 py-16">
+            <DialogActions className="px-8 py-16 justify-end gap-5">
                 <div className="px-16">
                     <Button type="submit" variant="contained" color="secondary" onClick={toggleDialog}>
-                        Cancel
+                        {t('CANCEL')}
                     </Button>
                 </div>
                 <div className="px-16">
                     <Button type="submit" variant="contained" color="secondary" onClick={toggleDialog}>
-                        Search
+                        {t('SEARCH')}
                     </Button>
                 </div>
             </DialogActions>
@@ -66,4 +68,5 @@ export default DateRangePicker;
 
 DateRangePicker.propTypes = {
     isOpen: PropTypes.bool.isRequired,
+    namespace: PropTypes.string.isRequired,
 };
