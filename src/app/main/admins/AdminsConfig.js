cd50382fbf8e51a7cf711ed8c2065c28fae26b6d
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-01 21:22:19 -0400

    Added spanish translations for Admins page

--- src/app/main/admins/AdminsConfig.js
+++ src/app/main/admins/AdminsConfig.js
@@ -1,10 +1,12 @@
 import i18next from 'i18next';
 import React from 'react';
 import en from './i18n/en';
+import es from './i18n/es';
 
-i18next.addResourceBundle('en', 'examplePage', en);
+i18next.addResourceBundle('en', 'admins', en);
+i18next.addResourceBundle('es', 'admins', es);
 
-const AdminsPageConfig = {
+const AdminsConfig = {
     settings: {
         layout: {
             config: {},
@@ -18,4 +20,4 @@ const AdminsPageConfig = {
     ],
 };
 
-export default AdminsPageConfig;
+export default AdminsConfig;
