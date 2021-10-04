0e807fad813121e32d5286451e74903180e14b89
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-04 11:33:23 -0400

    Some modifications on DriversMonitor page
    Redesigned the UI, fixing the rendering of Google Maps
    Translated the labels
    Removed the intermediate component DriversMonitorPage

--- src/app/main/drivers-monitor/DriversMonitorPageConfig.js
+++ src/app/main/drivers-monitor/DriversMonitorPageConfig.js
@@ -1,8 +1,10 @@
 import i18next from 'i18next';
 import React from 'react';
 import en from './i18n/en';
+import es from './i18n/es';
 
-i18next.addResourceBundle('en', 'examplePage', en);
+i18next.addResourceBundle('en', 'drivers-monitor', en);
+i18next.addResourceBundle('es', 'drivers-monitor', es);
 
 const DriversMonitorPageConfig = {
     settings: {
@@ -13,7 +15,7 @@ const DriversMonitorPageConfig = {
     routes: [
         {
             path: '/drivers_monitor',
-            component: React.lazy(() => import('./DriversMonitorPage')),
+            component: React.lazy(() => import('./DriversMonitor/DriversMonitor')),
         },
     ],
 };
---
+++ src/app/main/drivers-monitor/i18n/es.js
@@ -0,0 +1,5 @@
+const locale = {
+    DRIVERS_MONITOR: 'Monitoreo de Conductores',
+};
+
+export default locale;
