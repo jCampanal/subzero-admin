c4ca2e337f0d2a8910fe590962bd1b40d0b2517b
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-16 20:08:39 -0400

    Provided the whole app with the store

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ import './styles/app-base.css';
 import './styles/app-components.css';
 import './styles/app-utilities.css';
 import App from 'app/App';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'flag-icon-css/css/flag-icon.min.css';
+import store from './app/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 reportWebVitals();
 
