dccfeb70895a092b6570dff696de7e43eb96d5ff
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-16 20:04:24 -0400

    Added authorization reducer to rootReducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,11 +1,13 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import fuse from './fuse';
 import i18n from './i18nSlice';
+import authorization from './auth';
 
 const createReducer = (asyncReducers) => (state, action) => {
     const combinedReducer = combineReducers({
         fuse,
         i18n,
+        authorization,
         ...asyncReducers,
     });
 
