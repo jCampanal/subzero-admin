fb3d3939bea14110c2a80428a78796fcfe938a5a
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-10-20 14:31:11 -0400

    New service for updating categories

--- src/app/api-conn/categories/index.js
+++ src/app/api-conn/categories/index.js
@@ -6,3 +6,6 @@ export const getCategories = (pageSize = 10, pageNumber = 1) =>
     apiClient.get(`${categoryBase}?pageSize=${pageSize}&pageNumber=${pageNumber}`).then((response) => response.data);
 
 export const postCategory = (data) => apiClient.post(categoryBase, data, {headers: {'Content-Type': 'multipart/form-data'}}).then((response) => response);
+
+export const putCategory = (categoryId, data) =>
+    apiClient.put(`${categoryBase}/${categoryId}`, data, {headers: {'Content-Type': 'multipart/form-data'}}).then((response) => response);
