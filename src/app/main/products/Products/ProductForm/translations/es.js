9a9541ed39f43340bd0bbb1a9a0fd623dbb3c533
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-01 19:05:38 -0400

    Translation data files for ProductForm component
    Used in routes:
        '/product/create'
        '/product/{id}/edit'

---
+++ src/app/main/products/Products/ProductForm/translations/es.js
@@ -0,0 +1,24 @@
+const local = {
+    CREATE_NEW: 'Crear nuevo productos',
+    CREATING: 'Creando producto',
+    EDITING: 'Editando producto',
+    CREATE: 'Crear',
+    EDIT: 'Editar',
+    REMOVE: 'Eliminar',
+    SAVE: 'Guardar',
+    NAME: 'Nombre',
+    LINK: 'Enlace',
+    USE_DECIMALS: 'Acepta valores decimales',
+    DESCRIPTION: 'Descripción',
+    PRODUCT_UPDATED: 'El producto se ha actualizado exitosamente',
+    PRODUCT_UPDATE_ERROR: `Hemos tenido un problema tratando de actualizar el producto. Por favor inténtalo de nuevo más tarde o repórtalo al administrador del sistema`,
+    PRODUCT_CREATED: 'El producto se ha creado exitosamente',
+    PRODUCT_CREATE_ERROR:
+        'Hemos tenido un problema tratando de crear el producto. Por favor inténtalo de nuevo más tarde o repórtalo al administrador del sistema',
+    ADD_SALE_UNIT: 'Nueva Unidad de Venta',
+    VISIBLE: 'Visible',
+    CLICK_TO_LOAD: 'Haz click aquí para cargar una imagen',
+    CATEGORY: 'Categoría',
+};
+
+export default local;
