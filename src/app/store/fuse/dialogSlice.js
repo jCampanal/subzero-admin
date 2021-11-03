62a6cea68c2260b40cf3e1226ce4b7bb601ff267
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 19:09:13 -0400

    Fixed on dialogSlice from template core that when the closeDialog is dispatched the options get back to initial state

--- src/app/store/fuse/dialogSlice.js
+++ src/app/store/fuse/dialogSlice.js
@@ -1,24 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 
 const dialogSlice = createSlice({
-  name: 'dialog',
-  initialState: {
-    state: false,
-    options: {
-      children: 'Hi',
+    name: 'dialog',
+    initialState: {
+        state: false,
+        options: {
+            children: '',
+        },
     },
-  },
-  reducers: {
-    openDialog: (state, action) => {
-      state.state = true;
-      state.options = action.payload;
+    reducers: {
+        openDialog: (state, action) => {
+            state.state = true;
+            state.options = action.payload;
+        },
+        closeDialog: (state, action) => {
+            state.options = {children: ''};
+            state.state = false;
+        },
     },
-    closeDialog: (state, action) => {
-      state.state = false;
-    },
-  },
 });
 
-export const { openDialog, closeDialog } = dialogSlice.actions;
+export const {openDialog, closeDialog} = dialogSlice.actions;
 
 export default dialogSlice.reducer;
