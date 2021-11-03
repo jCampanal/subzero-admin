00aec6dbf2c9e3b8a7d5330cdb873f6185e28912
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 11:48:03 -0400

    Updated the Providers component to display the table of providers

--- src/app/main/providers/Providers/Providers.js
+++ src/app/main/providers/Providers/Providers.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {lazy, useEffect, useState} from 'react';
 import FusePageCarded from '@fuse/core/FusePageCarded';
 import {useSelector} from 'react-redux';
 import {useHistory} from 'react-router';
 import {useTranslation} from 'react-i18next';
 import {getProviders} from '../../../api-conn/providers';
+import tableRows from './tableRows';
 
 const Providers = () => {
     const history = useHistory();
@@ -29,6 +30,9 @@ const Providers = () => {
         loadProviders().finally();
     }, []);
 
+    const ProvidersTable = lazy(() => import('./ProvidersTable').then((table) => table));
+    const ProvidersHeader = lazy(() => import('../../products/Products/PageCardedHeader').then((header) => header));
+
     return (
         <FusePageCarded
             classes={{
@@ -36,8 +40,8 @@ const Providers = () => {
                 contentCard: 'overflow-hidden',
                 header: 'min-h-72 h-72 sm:h-136 sm:min-h-136',
             }}
-            header="Hello"
-            content="Hello"
+            header={<ProvidersHeader iconText="business" title="Providers" addButtonLabel="ADD" addButtonCallback={() => history.push('/providers/create')} />}
+            content={<ProvidersTable providers={providers} rows={tableRows} />}
             innerScroll
         />
     );
