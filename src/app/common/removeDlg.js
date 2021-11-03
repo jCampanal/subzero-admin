68fdadbfb2917eaf9f1e0400fa476904179eaeed
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 19:04:59 -0400

    Component removeDlg was made fully reusable

--- src/app/common/removeDlg.js
+++ src/app/common/removeDlg.js
@@ -2,32 +2,28 @@ import {DialogActions, DialogContent, DialogContentText, DialogTitle} from '@mat
 import Button from '@material-ui/core/Button';
 import React from 'react';
 import {useDispatch} from 'react-redux';
-import {deleteCooler} from '../api-conn/coolers';
 import {closeDialog} from '../store/fuse/dialogSlice';
 
 const RemoveDlg = (props) => {
     const dispatch = useDispatch();
-    const onProceed = () => {
-        const deleteItem = async () => {
-            await deleteCooler(props.itemId);
-        };
-        deleteItem().finally();
-        dispatch(closeDialog());
-        props.proceedCallback();
-    };
     return (
         <>
-            <DialogTitle id="alert-dialog-title">Use Google's location service?</DialogTitle>
+            <DialogTitle id="alert-dialog-title">{props.dlgTitle}</DialogTitle>
             <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
-                </DialogContentText>
+                <DialogContentText id="alert-dialog-description">{props.dlgText}</DialogContentText>
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => dispatch(closeDialog())} color="primary">
                     Cancel
                 </Button>
-                <Button onClick={() => onProceed()} color="primary" autoFocus>
+                <Button
+                    onClick={() => {
+                        props.proceedCallback();
+                        dispatch(closeDialog());
+                    }}
+                    color="primary"
+                    autoFocus
+                >
                     Proceed
                 </Button>
             </DialogActions>
