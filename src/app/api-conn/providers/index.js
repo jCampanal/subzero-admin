6f0bf5426de83f5447ddaa25f7c9fdda57044df7
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 10:14:57 -0400

    Added a service to get a paginated list of providers

--- src/app/api-conn/providers/index.js
+++ src/app/api-conn/providers/index.js
@@ -3,3 +3,6 @@ import apiClient from '../index';
 const providerUrl = '/admin/Provider';
 
 export const getProvidersAll = () => apiClient.get(`${providerUrl}/getAll`).then((response) => response.data);
+
+export const getProviders = (pageNumber = 0, pageSize = 10) =>
+    apiClient.get(`${providerUrl}?pageNumber=${pageNumber}&pageSize=${pageSize}`).then((response) => response.data);
