49562ddce53d5bbe49360ab9f5135c243e0285e1
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 00:20:26 -0400

    Extracted the row header definition for CoolersTable component to an independent file

---
+++ src/app/main/coolers/Coolers/tableRows.js
@@ -0,0 +1,46 @@
+const rows = [
+    {
+        id: 'image',
+        align: 'left',
+        disablePadding: true,
+        label: '',
+        sort: false,
+    },
+    {
+        id: 'code',
+        align: 'left',
+        disablePadding: false,
+        label: 'CODE',
+        sort: true,
+    },
+    {
+        id: 'provider',
+        align: 'left',
+        disablePadding: false,
+        label: 'PROVIDER',
+        sort: true,
+    },
+    {
+        id: 'status',
+        align: 'left',
+        disablePadding: false,
+        label: 'STATUS',
+        sort: true,
+    },
+    {
+        id: 'registration-date',
+        align: 'left',
+        disablePadding: false,
+        label: 'REGISTRATION_DATE',
+        sort: true,
+    },
+    {
+        id: 'actions',
+        align: 'right',
+        disablePadding: false,
+        label: '',
+        sort: false,
+    },
+];
+
+export default rows;
