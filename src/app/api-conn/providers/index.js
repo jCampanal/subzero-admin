977f4027e3f813f2d79041cd54cc2840fc1e05b7
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 19:11:22 -0400

    Created new services to interact with providers data on API
    postProviders for creating
    putProviders for updating
    deleteProviders for deleting

--- src/app/api-conn/providers/index.js
+++ src/app/api-conn/providers/index.js
@@ -6,3 +6,9 @@ export const getProvidersAll = () => apiClient.get(`${providerUrl}/getAll`).then
 
 export const getProviders = (pageNumber = 0, pageSize = 10) =>
     apiClient.get(`${providerUrl}?pageNumber=${pageNumber}&pageSize=${pageSize}`).then((response) => response.data);
+
+export const postProviders = (data) => apiClient.post(providerUrl, data).then((response) => response.data);
+
+export const putProviders = (id, data) => apiClient.put(`${providerUrl}/${id}`, data).then((response) => response.data);
+
+export const deleteProviders = (id) => apiClient.delete(`${providerUrl}/${id}`).then((response) => response.data);
