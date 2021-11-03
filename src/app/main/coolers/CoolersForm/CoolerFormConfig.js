e80992d889602651f912fa7ca42327b4926170d5
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 00:09:40 -0400

    Configurations for CoolerForm component

---
+++ src/app/main/coolers/CoolersForm/CoolerFormConfig.js
@@ -0,0 +1,29 @@
+import i18next from 'i18next';
+import React from 'react';
+import en from './translations/en';
+import es from './translations/es';
+
+i18next.addResourceBundle('en', 'coolers-form', en);
+i18next.addResourceBundle('es', 'coolers-form', es);
+
+const CoolersPageConfig = {
+    settings: {
+        layout: {
+            config: {},
+        },
+    },
+    routes: [
+        {
+            path: '/coolers/create',
+            exact: true,
+            component: React.lazy(() => import('./CoolerForm').then((form) => form)),
+        },
+        {
+            path: '/coolers/:id/edit',
+            exact: true,
+            component: React.lazy(() => import('./CoolerForm').then((form) => form)),
+        },
+    ],
+};
+
+export default CoolersPageConfig;
