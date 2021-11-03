a94ed3862dd2065ab1dafddf695b166ed9320a30
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-03 19:09:53 -0400

    Added some translation messages to the Providers component

--- src/app/main/providers/Providers/translations/en.js
+++ src/app/main/providers/Providers/translations/en.js
@@ -1,3 +1,9 @@
-const locale = {};
+const locale = {
+    DELETE_SUCCESSFUL: 'The provider was deleted successfully',
+    DELETE_ERROR: 'We had an error trying to delete the provider. Please try again later or report it to system administrator',
+    WARNING_TITLE: 'Warning, you have requested a risky operation',
+    WARNING_TEXT: 'You are attempting to delete a provider, this operation cannot be undone. Are you sure you want to proceed with the deletion?',
+    ADD: 'Add Provider',
+};
 
 export default locale;
