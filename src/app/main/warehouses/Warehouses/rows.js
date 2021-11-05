c8eac73883d736c41a8c156c35d933df37bf96f6
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-04 23:27:38 -0400

    Extracted warehouses' table column names to its own file

---
+++ src/app/main/warehouses/Warehouses/rows.js
@@ -0,0 +1,25 @@
+const rows = [
+    {
+        id: 'image',
+        align: 'left',
+        disablePadding: true,
+        label: '',
+        sort: false,
+    },
+    {
+        id: 'name',
+        align: 'left',
+        disablePadding: false,
+        label: 'NAME',
+        sort: true,
+    },
+    {
+        id: 'actions',
+        align: 'right',
+        disablePadding: false,
+        label: '',
+        sort: false,
+    },
+];
+
+export default rows;
