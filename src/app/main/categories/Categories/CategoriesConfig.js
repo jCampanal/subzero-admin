d4dff7c09b20260282612e533323171ae5e31683
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-09 14:08:14 -0500

    Moved Categories component's configuration files into 'Categories' folder
    Includes route configuration and translations data

--- src/app/main/categories/Categories/CategoriesConfig.js
+++ src/app/main/categories/Categories/CategoriesConfig.js
@@ -6,7 +6,7 @@ import es from './translations/es';
 i18next.addResourceBundle('en', 'categories', en);
 i18next.addResourceBundle('es', 'categories', es);
 
-const CategoriesPageConfig = {
+const CategoriesConfig = {
     settings: {
         layout: {
             config: {},
@@ -16,9 +16,9 @@ const CategoriesPageConfig = {
         {
             path: '/categories',
             exact: true,
-            component: React.lazy(() => import('./Categories/Categories')),
+            component: React.lazy(() => import('./Categories').then((categories) => categories)),
         },
     ],
 };
 
-export default CategoriesPageConfig;
+export default CategoriesConfig;
