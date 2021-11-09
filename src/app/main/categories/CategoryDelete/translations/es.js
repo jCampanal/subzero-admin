4c15ae54abf8510a3efe8f5f66b1719f0b2a3519
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-09 18:49:39 -0500

    Deleted some unused components

--- src/app/main/categories/CategoryDelete/translations/es.js
+++
@@ -1,9 +0,0 @@
-const local = {
-    ARE_YOU_SURE: '¿Seguro que quieres eliminar esta categoría? Ten en cuenta que esta operación no puede deshacerse',
-    CANCEL: 'Cancelar',
-    DELETE: 'Eliminar',
-    CATEGORIES: 'Categorías',
-    PRODUCTS: 'Un producto',
-    PRODUCTS_plural: '{{count}} products',
-};
-export default local;
