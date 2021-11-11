3ac38233408ad2332fbf59e53ae5809f77ad05eb
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-11 12:20:52 -0500

    Moved 'getSaleUnit' service from product services to saleUnit services

--- src/app/api-conn/products/index.js
+++ src/app/api-conn/products/index.js
@@ -2,12 +2,12 @@ import apiClient from '../index';
 
 const productsBase = '/admin/Product';
 
-export const getProducts = (pageSize = 10, pageNumber = 0) => apiClient.get(productsBase, {params: {pageSize, pageNumber}}).then((response) => response.data);
+const getProducts = (pageSize = 10, pageNumber = 0) => apiClient.get(productsBase, {params: {pageSize, pageNumber}}).then((response) => response.data);
 
-export const getProductById = (id) => apiClient.get(`${productsBase}/${id}`).then((response) => response.data);
+const getProductById = (id) => apiClient.get(`${productsBase}/${id}`).then((response) => response.data);
 
-export const postProduct = (data) => apiClient.post(productsBase, data).then((response) => response.data);
+const postProduct = (data) => apiClient.post(productsBase, data).then((response) => response.data);
 
-export const putProduct = (id, data) => apiClient.put(`${productsBase}/${id}`, data).then((response) => response.data);
+const putProduct = (id, data) => apiClient.put(`${productsBase}/${id}`, data).then((response) => response.data);
 
-export const getSaleUnits = () => apiClient.get(`${productsBase}/saleUnit`).then((response) => response.data);
+export {getProducts, getProductById, postProduct, putProduct};
--- src/app/api-conn/products/sale-units/index.js
+++ src/app/api-conn/products/sale-units/index.js
@@ -2,6 +2,8 @@ import apiClient from '../../index';
 
 const saleUnitBaseUrl = '/admin/Product/saleUnit';
 
+export const getSaleUnits = () => apiClient.get(saleUnitBaseUrl).then((response) => response.data);
+
 export const postSaleUnit = (data) => apiClient.post(saleUnitBaseUrl, data).then((response) => response.data);
 
 export const putSaleUnit = (data) => apiClient.put(saleUnitBaseUrl, data).then((response) => response.data);
