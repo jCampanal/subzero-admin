50a336791ede4041ac048e2edefe536329843c7e
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-11 09:56:46 -0500

    Extracted the column names definition for products tabla to an isolated file

---
+++ src/app/common/productRows.js
@@ -0,0 +1,46 @@
+const rows = [
+    {
+        id: 'image',
+        align: 'left',
+        disablePadding: true,
+        label: '',
+        sort: false,
+    },
+    {
+        id: 'name',
+        align: 'left',
+        disablePadding: false,
+        label: 'NAME',
+        sort: true,
+    },
+    {
+        id: 'category',
+        align: 'left',
+        disablePadding: false,
+        label: 'CATEGORY',
+        sort: true,
+    },
+    {
+        id: 'units',
+        align: 'right',
+        disablePadding: false,
+        label: 'UNITS',
+        sort: true,
+    },
+    {
+        id: 'visible',
+        align: 'right',
+        disablePadding: false,
+        label: 'VISIBLE',
+        sort: true,
+    },
+    {
+        id: 'actions',
+        align: 'right',
+        disablePadding: false,
+        label: '',
+        sort: false,
+    },
+];
+
+export default rows;
