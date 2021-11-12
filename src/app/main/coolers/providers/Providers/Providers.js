be0625e92eb929099f171e4714666f046c13905d
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-12 18:55:15 -0500

    Improvements to Providers component
    - Show message when the 'loadProviders' operation fails

--- src/app/main/coolers/providers/Providers/Providers.js
+++ src/app/main/coolers/providers/Providers/Providers.js
@@ -1,4 +1,4 @@
-import React, {lazy, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import FusePageCarded from '@fuse/core/FusePageCarded';
 import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from 'react-router';
@@ -9,34 +9,36 @@ import rows from './rows';
 import {openDialog} from '../../../../store/fuse/dialogSlice';
 import RemoveDlg from '../../../../common/removeDlg';
 import {showMessage} from '../../../../store/fuse/messageSlice';
+import PageCardedHeader from '../../../products/Products/PageCardedHeader';
+import ProvidersTable from './ProvidersTable';
 
 const Providers = () => {
     const history = useHistory();
+    const dispatch = useDispatch();
     const {
         user: {logged},
     } = useSelector((state) => state);
-    if (!logged) history.push('/login');
-
     const {t} = useTranslation('providers');
-    useEffect(() => {
-        document.title = t('PAGE_TITLE');
-    }, [t]);
     const [loading, setLoading] = useState(false);
     const [providers, setProviders] = useState([]);
 
-    const loadProviders = async () => {
+    const loadProviders = () => {
         setLoading(true);
-        const {data} = await getProviders();
-        setProviders(data);
-        setLoading(false);
+        getProviders()
+            .then((response) => {
+                setProviders(response.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                dispatch(
+                    showMessage({
+                        message: 'Something went wrong. Try to reload the page',
+                        variant: 'error',
+                    })
+                );
+            });
     };
-    useEffect(() => {
-        loadProviders().finally();
-    }, []);
-
     const showProvider = (provider) => history.push(`/providers/${provider.id}`, {provider});
-
-    const dispatch = useDispatch();
     const onProceed = (id) => {
         setLoading(true);
         deleteProviders(id)
@@ -52,7 +54,7 @@ const Providers = () => {
                     })
                 );
                 setLoading(false);
-                loadProviders().finally();
+                loadProviders();
             })
             .catch(() => {
                 setLoading(false);
@@ -68,7 +70,6 @@ const Providers = () => {
                 );
             });
     };
-
     const editProvider = (provider) => history.push(`/providers/${provider.id}/edit`, {provider});
     const deleteProvider = (provider) =>
         dispatch(
@@ -79,8 +80,13 @@ const Providers = () => {
             })
         );
 
-    const ProvidersTable = lazy(() => import('./ProvidersTable').then((table) => table));
-    const ProvidersHeader = lazy(() => import('../../../products/Products/PageCardedHeader').then((header) => header));
+    useEffect(() => {
+        if (!logged) history.push('/login');
+    }, [logged, history]);
+    useEffect(() => {
+        document.title = t('PAGE_TITLE');
+    }, [t]);
+    useEffect(loadProviders, [dispatch]);
 
     return (
         <FusePageCarded
@@ -90,7 +96,7 @@ const Providers = () => {
                 header: 'min-h-72 h-72 sm:h-136 sm:min-h-136',
             }}
             header={
-                <ProvidersHeader
+                <PageCardedHeader
                     iconText="business"
                     title="Providers"
                     addButtonLabel={t('ADD')}
