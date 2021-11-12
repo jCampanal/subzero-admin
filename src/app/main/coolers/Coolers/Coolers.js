d8d424a3aec869d0a16cf84cbaf39b7e31df3cc4
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-12 18:04:20 -0500

    Added horizontal margin to coolers table

--- src/app/main/coolers/Coolers/Coolers.js
+++ src/app/main/coolers/Coolers/Coolers.js
@@ -85,7 +85,7 @@ function Coolers() {
     return (
         <FusePageCarded
             classes={{
-                content: 'flex',
+                content: 'flex mx-14',
                 contentCard: 'overflow-hidden',
                 header: 'min-h-72 h-72 sm:h-136 sm:min-h-136',
             }}
