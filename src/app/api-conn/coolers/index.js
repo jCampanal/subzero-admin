eae3b45efebd7e6ea5d26a764ade5c8a818d03fd
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-12 17:16:40 -0500

    Fixed some issues with the 'getCoolers' service

--- src/app/api-conn/coolers/index.js
+++ src/app/api-conn/coolers/index.js
@@ -2,7 +2,7 @@ import apiClient from '../index';
 
 const coolersUrl = '/admin/Coolers';
 
-const getCoolers = (pageNumber = 0, pageSize = 10, code = '', date = '', returned = undefined) =>
+const getCoolers = (pageNumber = 0, pageSize = 10, code = undefined, date = undefined, returned = undefined) =>
     apiClient.get(coolersUrl, {params: {pageSize, pageNumber, code, date, returned}}).then((response) => response.data);
 
 const postCooler = (data) => apiClient.post(coolersUrl, data, {headers: {'Content-Type': 'multipart/form-data'}}).then((response) => response.data);
