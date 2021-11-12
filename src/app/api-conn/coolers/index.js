ab7f6d30e0a5506d30be96a44529052874387f42
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2021-11-12 16:49:51 -0500

    Updated the coolers services
    - Added the filter parameters to 'getCoolers'
    - Fixed the signatura for 'deleteCooler'

--- src/app/api-conn/coolers/index.js
+++ src/app/api-conn/coolers/index.js
@@ -2,12 +2,14 @@ import apiClient from '../index';
 
 const coolersUrl = '/admin/Coolers';
 
-export const getCoolers = (pageNumber = 0, pageSize = 10) =>
-    apiClient.get(`${coolersUrl}?pageSize=${pageSize}&pageNumber=${pageNumber}`).then((response) => response.data);
+const getCoolers = (pageNumber = 0, pageSize = 10, code = '', date = '', returned = undefined) =>
+    apiClient.get(coolersUrl, {params: {pageSize, pageNumber, code, date, returned}}).then((response) => response.data);
 
-export const postCooler = (data) => apiClient.post(coolersUrl, data, {headers: {'Content-Type': 'multipart/form-data'}}).then((response) => response.data);
+const postCooler = (data) => apiClient.post(coolersUrl, data, {headers: {'Content-Type': 'multipart/form-data'}}).then((response) => response.data);
 
-export const putCooler = (id, data) =>
+const putCooler = (id, data) =>
     apiClient.put(`${coolersUrl}/${id}`, data, {headers: {'Content-Type': 'multipart/form-data'}}).then((response) => response.data);
 
-export const deleteCooler = (id) => apiClient.delete(`${coolersUrl}/${id}`).then((response) => response.data);
+const deleteCooler = (ids) => apiClient.delete(coolersUrl, {data: ids}).then((response) => response.data);
+
+export {getCoolers, postCooler, putCooler, deleteCooler};
