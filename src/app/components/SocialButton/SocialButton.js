1986d23a8935c20d85f64bffaf263012d6b9fda8
Cesar <cesrmartn@yahoo.es>
2021-12-20 01:14:03 -0500

    Open in another tab link of social media

--- src/app/components/SocialButton/SocialButton.js
+++ src/app/components/SocialButton/SocialButton.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const SocialButton = ({ link, icon, backColor, textColor }) => {
   return (
-    <a href={link} className="m-12 cursor-pointer">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="m-12 cursor-pointer"
+    >
       <div
         className="w-52 h-52 flex justify-center items-center text-white rounded-md text-24"
         style={{ backgroundColor: backColor, color: textColor }}
