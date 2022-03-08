1496b2e4225488d90c700ffc27f68cd9c3486869
JCampanal <jcampanal08@gmail.com>
2022-03-07 20:19:57 -0500

    resuelto visual del driver monitor

--- src/app/main/drivers-monitor/DriversMonitor/SelectDriversBar/DriversElements/DriversElements.js
+++ src/app/main/drivers-monitor/DriversMonitor/SelectDriversBar/DriversElements/DriversElements.js
@@ -1,45 +1,43 @@
-import React from "react";
-import DriverElement from "./DriverElement/DriverElement";
-import { setDrivers } from "app/store/driverMonitor/driverMonitor";
+import React from 'react'
+import DriverElement from './DriverElement/DriverElement'
+import { setDrivers} from "app/store/driverMonitor/driverMonitor";
 import { useDispatch } from "react-redux";
 
-const DriversElements = (props) => {
-  const dispatch = useDispatch();
 
-  const handlerChangeCheckbox = (id) => {
-    let changedDriver = props.Drivers.find((element) => {
-      return element.id === id;
-    });
-    if (changedDriver) {
-      changedDriver = { ...changedDriver, enable: !changedDriver.enable };
-      const newDrivers = props.Drivers.map((element) => {
-        if (element.id === changedDriver.id) {
-          return changedDriver;
-        } else {
-          return element;
+const DriversElements=(props)=>{
+    const dispatch=useDispatch()
+
+    const handlerChangeCheckbox=(id)=>{
+
+        let changedDriver=props.Drivers.find(element=>{return(element.id===id)})
+        if(changedDriver){
+            changedDriver={...changedDriver,enable:!changedDriver.enable}
+            const newDrivers=props.Drivers.map(element=>{
+                if(element.id===changedDriver.id){
+                    return changedDriver
+                }else{
+                    return element
+                }
+            })
+            console.log(newDrivers)
+            dispatch(setDrivers(newDrivers))
         }
-      });
-      console.log(newDrivers);
-      dispatch(setDrivers(newDrivers));
     }
-  };
-  const MDrivers = props.Drivers.map((element) => {
-    return (
-      <DriverElement
-        key={element.id}
-        Enable={element.enable}
-        Id={element.id}
-        ClickEnable={() => {}}
-        Name={element.name}
-        Online={element.online}
-        Color={element.color}
-        Ordenes={"2"}
-        Stops={["Parque El Curita", "Santiago de las vegas"]}
-        Click={() => handlerChangeCheckbox(element.id)}
-      />
-    );
-  });
-  return MDrivers;
-};
+    let MDrivers=props.Drivers.map(element=>{
+        return(
+           <DriverElement key={element.id} 
+                          Enable={element.enable}
+                          Id={element.id}
+                          ClickEnable={()=>{}}
+                          Name={element.name}
+                          Online={element.online}
+                          Color={element.color}
+                          Ordenes={'2'}
+                          Stops={['Parque El Curita','Santiago de las vegas']}
+                          Click={()=>handlerChangeCheckbox(element.id)}/>
+        )
+    })
+    return(MDrivers)
+}
 
-export default DriversElements;
+export default DriversElements
--- src/app/main/drivers-monitor/DriversMonitor/SelectDriversBar/SelectDriversBar.js
+++ src/app/main/drivers-monitor/DriversMonitor/SelectDriversBar/SelectDriversBar.js
@@ -1,33 +1,38 @@
-import React, { Fragment } from "react";
-import {
-  BarDriversPrincipalS,
-  BarDriversSecndari1S,
-  LabelS,
-} from "./SelectDriversBar.style";
-import DriversElements from "./DriversElements/DriversElements";
+import React,{useState,Fragment, useEffect} from 'react'
+import { BarDriversPrincipalS,BarDriversSecndari2S,BarDriversSecndari1S,LabelS } from "./SelectDriversBar.style";
+import DriversElements from "./DriversElements/DriversElements"
 import FuseScrollbars from "@fuse/core/FuseScrollbars";
 import { useSelector } from "react-redux";
-import { getDriversCarded } from "app/store/driverMonitor/driverMonitor";
-import AirportShuttleIcon from "@material-ui/icons/AirportShuttle";
-const SelectDriversBar = (props) => {
-  const ShowCarded = useSelector(getDriversCarded);
-  console.log(ShowCarded);
+import { getDriversCarded} from "app/store/driverMonitor/driverMonitor";
+import AirportShuttleIcon from '@material-ui/icons/AirportShuttle';
+const SelectDriversBar =(props)=>{
 
-  return (
-    <Fragment>
-      <BarDriversPrincipalS>
-        <LabelS>
-          <AirportShuttleIcon className="mr-5" fontSize="large" />
-          <h1>Drivers:</h1>
-        </LabelS>
-        <FuseScrollbars className="flex-grow overflow-y-auto ">
-          <BarDriversSecndari1S>
-            <DriversElements Drivers={props.Drivers} />
-          </BarDriversSecndari1S>
-        </FuseScrollbars>
-      </BarDriversPrincipalS>
-    </Fragment>
-  );
-};
+    const ShowCarded=useSelector(getDriversCarded)
+    console.log(ShowCarded)
 
-export default SelectDriversBar;
+    
+
+    return(
+        <Fragment>
+        < BarDriversPrincipalS>
+            
+            <LabelS>
+                <AirportShuttleIcon className="mr-5" fontSize="large" />
+                <h1>Drivers:</h1>
+            </LabelS>
+            <FuseScrollbars className="flex-grow overflow-y-auto ">
+                <BarDriversSecndari1S>
+                
+                <DriversElements Drivers={props.Drivers}/> 
+                
+                </BarDriversSecndari1S>
+            </FuseScrollbars>
+       
+        </BarDriversPrincipalS>               
+        </Fragment>
+        
+        
+    )
+}
+
+export default SelectDriversBar
